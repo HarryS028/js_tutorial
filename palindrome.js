3dff40f0c991a16b22f3496ccb2a20e6d12a4858
EC2 Default User <ec2-user@ip-172-31-28-55.us-east-2.compute.internal>
2019-06-26 13:25:17 +0000

    Create live palindrome application

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,40 @@
-//Reverses a string
-function reverse(string) {
-    return Array.from(string).reverse().join("");
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
-// Returns true for a palindrome, false otherwise
-function palindrome(string) {
-    let processedContent = string.toLowerCase();
-    return processedContent === reverse(processedContent);
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+  
+  this.processor = function(string) {
+    return this.content.toLowerCase();
+  }
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+  
+  // Makes the phrase LOUDER.
+  this.louder = function() {
+    let loudcontent = this.content.toUpperCase();
+    return loudcontent;
+  };
+}
+
+//Defines a translated phrase object
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+  
+  // Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+  return this.processor(this.translation);
+  }
 }
